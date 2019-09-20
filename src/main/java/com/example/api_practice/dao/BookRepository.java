87ad5422bfa38532977ccc3be58ca14e812c13dc
jamespengthinkpower <james.peng@thinkpower-info.com>
2019-09-20 17:41:10 +0800

    api +swagger

---
+++ src/main/java/com/example/api_practice/dao/BookRepository.java
@@ -0,0 +1,29 @@
+package com.example.api_practice.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.example.api_practice.model.Book;
+@RepositoryRestResource
+public interface BookRepository extends JpaRepository<Book, Integer> {
+	
+    // Prevents GET /books/:id
+    @Override
+    Book getOne(Integer id);
+
+    // Prevents GET /books
+    @Override
+    Page<Book> findAll(Pageable pageable);
+
+    // Prevents POST /books and PATCH /books/:id
+    @Override
+    Book save(Book s);
+
+    // Prevents DELETE /books/:id
+    @Override
+    @RestResource(exported = false)
+    void delete(Book t);
+}
